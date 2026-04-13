e1ee580c0b37f268b3b30b617a1daa01d9824aff
Prantik Kumar Seal <83081442+prantikseal@users.noreply.github.com>
2026-04-13 11:23:13 +0530

    cleanup team section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,7 +18,7 @@ export default function AboutPage() {
       <MissionVisionSection />
       <UnlockGrowthSection />
       <FounderSection />
-      <AboutTeamSection />
+      {/* <AboutTeamSection /> */}
       <ReadyToElevateSection />
     </main>
   );
